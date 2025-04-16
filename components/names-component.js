f5d6a5a23f955ba198f2e91283cdfc71e0a7b571
Marek Kosmowski <kosmowski.marek@gmail.com>
2025-04-16 22:58:11 +0200

    Add defaults state for names component, add input and button elements

--- components/names-component.js
+++ components/names-component.js
@@ -1,5 +1,44 @@
-import { Text } from 'react-native';
+import { Text, View, TextInput, StyleSheet, Button, Alert } from 'react-native';
+import { useState } from 'react';
+import { AppStyles } from '../constants/styles';
+
+const DEFAULTS = {
+  modal: false,
+  inputName: '',
+  names: [],
+  result: '',
+};
 
 export default function NamesComponent() {
-  return <Text>This is where names will be displayed</Text>;
+  const [state, setState] = useState(DEFAULTS);
+
+  const handleInput = (value) => setState({ ...state, inputName: value });
+
+  return (
+    <View style={{ width: '80%' }}>
+      <>
+        <TextInput
+          value={state.inputName}
+          onChangeText={(value) => {
+            handleInput(value);
+          }}
+          style={styles.input}
+          placeholder='provide a name'
+        />
+        <Button title='Add name' onPress={() => Alert.alert('Hello !')} />
+      </>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 8,
+    fontSize: 20,
+    marginBottom: 20,
+    borderColor: AppStyles.color.indigoInk,
+    borderWidth: 1,
+    padding: 12,
+    backgroundColor: AppStyles.color.white,
+  },
+});
